d41898fc815bc07acf945cf19da30b99af0de247
paulorc1964 <paulorobertocosta@hotmail.com>
2023-09-04 11:44:38 -0300

    first commit

---
+++ bin/adf_cli.dart
@@ -0,0 +1,5 @@
+import 'package:adf_cli/adf_cli.dart' as adf_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${adf_cli.calculate()}!');
+}
---
+++ test/adf_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:adf_cli/adf_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
